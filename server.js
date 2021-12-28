9e755370d8877da76b5816f6f1a459c65d219d64
Jose Gabriel Jiménez Vidales <josegabojimenez@gmail.com>
2021-12-28 17:22:00 -0500

    Deployment on heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001; //? PORT
+const PORT = process.env.PORT || 3001; //? PORT
 const routerApi = require('./routes'); //? Router
 
 const {
