183b8dee701a604334af9262f6d11d8ee4e2a75a
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-01-18 11:40:40 -0500

    Nodemailer testing (You can send emails from the app! :D)

---
+++ nodemailer.js
@@ -0,0 +1,35 @@
+'use strict';
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendEmail() {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `"Jose Gabo 👻" <${process.env.EMAIL_USER}>`, // sender address
+    to: `${process.env.EMAIL_USER}`, // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<h1><b>This is kinda crazy</b></h1>', // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+sendEmail();
